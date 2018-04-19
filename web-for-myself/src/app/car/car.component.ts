2595345d8952e7a7f21186cd3ce6f1b06334519f
Sensdoo <sensibleprogrammer@gmail.com>
2018-04-19 15:17:42 +0300

    add project

---
+++ web-for-myself/src/app/car/car.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-car',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css']
+})
+export class CarComponent implements OnInit {
+
+  @Input() car: {name: string, year: number};
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
---
+++ web-for-myself/src/app/cars/cars.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cars',
+  templateUrl: './cars.component.html',
+  styleUrls: ['./cars.component.css']
+})
+export class CarsComponent implements OnInit {
+
+
+  cars: [{name: string, year: number}] = [
+    {name: 'BMW', year: 2017},
+    {name: 'Ford', year: 2015},
+    {name: 'Mercedes', year: 2016},
+    {name: 'Mazda', year: 2011}
+  ]
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+  getDate(): Date {
+    return new Date();
+  }
+
+  addCar(car: {name: string, year: number }) {
+    this.cars.push(car);
+  }
+}
